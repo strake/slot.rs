79f0236f4ec06e36693ec315b4dc74e377aa8028
M Farkas-Dyck <strake888@gmail.com>
2018-01-14 14:31:20 -0500

    publish member

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,11 @@ use core::mem;
 
 #[allow(unions_with_drop_fields)]
 #[derive(Clone, Copy)]
-pub union Slot<T> { x: T }
+pub union Slot<T> { pub x: T }
 
 impl<T> Slot<T> {
     #[inline]
     pub fn new() -> Self { unsafe { mem::uninitialized() } }
-
-    #[inline]
-    pub unsafe fn get(self) -> T { self.x }
-
-    #[inline]
-    pub unsafe fn as_ref(&self) -> &T { &self.x }
-
-    #[inline]
-    pub unsafe fn as_mut(&mut self) -> &mut T { &mut self.x }
 }
 
 impl<T> From<T> for Slot<T> {
